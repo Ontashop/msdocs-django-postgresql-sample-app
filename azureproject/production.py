dcd9cf8c2bd79ed6bfd1ecfd6458f93a15713f78
Ontashop <45619492+Ontashop@users.noreply.github.com>
2025-03-21 12:26:12 +0000

    Configure Azure database and cache connectons

--- azureproject/production.py
+++ azureproject/production.py
@@ -27,6 +27,7 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
